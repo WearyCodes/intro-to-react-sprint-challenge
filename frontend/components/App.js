f3eee7562e6d947c15524e7813289f55b2327547
Samuel Sanchez <wearycodes@gmail.com>
2023-09-18 22:55:42 -0500

    im so stuck

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,7 +7,47 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+
+  const [people, setPeople] = useState([])
+  const [planets, setPlanets] = useState([])
+
   // ❗ Create effects to fetch the data and put it in state
+
+  useEffect(() => {
+    axios.get(urlPeople)
+    .then(res => {
+      setPeople(res.data)
+    })
+    .catch(err => console.log(err))
+    axios.get(urlPlanets)
+    .then(res => {
+      setPlanets(res.data)}
+    )
+      .catch(err => console.log(err))
+  }, [])
+  
+  if (people){console.log(people)}
+  if (planets){console.log(planets)}
+const peopleArray = []
+  planets.forEach(planet => {
+    people.forEach(person => {
+      if (person.homeworld === planet.id){console.log(person.name, planet.name)}
+    })
+  })
+
+
+
+  people.forEach(person => {
+    peopleArray.push({
+      id: person.id,
+      name: person.name,
+      homeworld: {
+        name: 'vas',
+        id: 'vas'
+      }
+    })
+  }, [])
+console.log(peopleArray)
   return (
     <div>
       <h2>Star Wars Characters</h2>
