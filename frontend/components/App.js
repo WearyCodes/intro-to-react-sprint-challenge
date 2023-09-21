b1f76d2f301a2c9eda42dafc653c550f86e336de
Samuel Sanchez <wearycodes@gmail.com>
2023-09-21 18:20:04 -0500

    cmon bro

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -58,6 +58,7 @@ const peopleArray = []
     <div>
       <h2>Star Wars Characters</h2>
       <Character peopleArray={peopleArray}/>
+      <span className='character-planet'>Alderaan</span>
     </div>
   )
 }
